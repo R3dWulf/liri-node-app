2132fedcf1ea1105bae0ee32cdb7beadaef8438f
R3dWulf <theakaiokami@gmail.com>
2017-07-17 22:41:57 -0400

    commiting push

---
+++ keys.js
@@ -0,0 +1,15 @@
+//```JavaScript
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'WAewJvQE5vV23h8X6eYrka8wh',
+  consumer_secret: 'y5OsHDZ5lFlswqPgrRSpWJFfa1ajiKQkmlDYBUnLRI6eTPj3Q0',
+  access_token_key: '275305362-kJw8ZPL48OCnKrqJJL4nSd5taoHVzjhSxBQbh4lo',
+  access_token_secret: 'ND2Tkb0N3NT974FFq6BwMT3LN0rE3jDnmGNP7fESBHqgM'
+};
+
+exports.spotifyKeys = {
+	client_id: "8bb7c5b56e9c49dfb4cc50bae38d2e2f",
+	client_secret: "cb6ac225faa347f68126dd6ff19cc9f7"
+};
+//```
--- liri.js
+++ liri.js
@@ -20,21 +20,33 @@ var input = process.argv[2];
 
 var songName = "";
 
+var noSong = "";
+
+var doWhat = false
+
 
 //**************************************************Functions****************************************************
-switch (input) {
-	case "my-tweets" : myTweets(); 
-	break;
+	switch (input) {
+		case "my-tweets" : myTweets(); 
+		break;
 
-	case "movie-this" : movieThis();
-	break;
+		case "movie-this" : movieThis();
+		break;
 
-	//case "spotify-this-song": spotifyThisSong(); 
-	//break;
+		case "spotify-this-song": spotifyThisSong(); 
+		break;
 
-	case "do-what-it-says": doWhatItSays(); 
-	break;
-}
+		case "do-what-it-says": doWhatItSays(); 
+		break;
+
+		default: console.log("\r\n" +"Try one of the following commands after 'node liri.js'" +"\r\n"+
+				"1. my-tweets, 'to see my latest tweets'" +"\r\n"+
+				"2. spotify-this-song + 'any song name here' "+"\r\n"+
+				"3. movie-this + 'any movie name here' "+"\r\n"+
+				"4. do-what-it-says."+"\r\n"
+				);
+	}
+
 //Pull 20 tweets from twitter
 function myTweets() {
 
@@ -47,6 +59,7 @@ function myTweets() {
 	     	//will need a for loop
 	     	for (var i = 0; i < tweets.length; i++){
 	    	    		console.log(tweets[i].text);
+	    	    		log(tweets[i].text);
 	     	}
 		}
 	});
@@ -109,7 +122,16 @@ function movieThis(){
 
 				);
 
+				var mrNobodyLog = 
+
+					"If you have not seen Mr Nobody, you should," + "\r\n" +
+
+					"its on Netflix and can be found on IMBd here: http://www.imdb.com/title/tt0485947/";
+
+				log(mrNobodyLog);
+
 			}
+
 
 	     	console.log(
 
@@ -120,11 +142,26 @@ function movieThis(){
  	    			"IMDB rating of the movie: " + movieObject.imdbRating
  	    		);
 
+ 	    		var moviePart1Log = 
+
+	     		"Title of the movie: " + movieObject.Title + "\r\n" +
+
+	     		"Year the movie came out: " + movieObject.Released + "\r\n" +
+
+ 	    			"IMDB rating of the movie: " + movieObject.imdbRating;
+
+ 	    		log(moviePart1Log);
+
 			for (var i = 0; i < movieObject.Ratings.length; i++){ 
    			
 
 				if ( movieObject.Ratings[i].Source == "Rotten Tomatoes") {
+
 					console.log("Rotten Tomatoes Rating: " + movieObject.Ratings[i].Value);
+
+					var rottenTomatoesLog = "Rotten Tomatoes Rating: " + movieObject.Ratings[i].Value;
+
+					log(rottenTomatoesLog);
 				}
 
  	     	}
@@ -143,12 +180,25 @@ function movieThis(){
 
 		);
 
+		var moviePart2Log = 
+
+			"Country where the movie was produced: " + movieObject.Country + "\r\n" +
+
+			"Language of the movie: " + movieObject.Language + "\r\n" +
+
+			"Plot of the movie: " + movieObject.Plot + "\r\n" +
+
+			"Actors in the movie: " + movieObject.Actors;
+
+			log(moviePart2Log);
+
 	// End request function
 	});
 // End movieThis function
 }
 
-function doWhatItSays(){
+
+function spotifyThisSong(){
 
 	var clientId = neededKeys.spotifyKeys.client_id;
 
@@ -159,23 +209,42 @@ function doWhatItSays(){
 		secret: clientSecret
 	});
 
-	fs.readFile("random.txt", "utf8", function(error, data){
+	var songTitle = process.argv;
 
-		if (error) {
-			return console.log(error);
-			}
+	if ( doWhat === false ){
 
+		var noSong = process.argv[3];
 
-			var readFileArr = data.split(",");
-
-			//console.log(readFileArr[1]);
-
-			songName = readFileArr[1];
+	} 
 
 
-			spotify.search({type: 'track' , query: songName}, function(error,data,response){
+	for ( var i = 3; i < songTitle.length; i++ ) {	
 
-			console.log(
+		if ( i > 3 && i < songTitle.length) {
+
+			//movieParamsName = movieParamsName + "+" + movieTitle[i];
+			songName = songName + " " + songTitle[i];
+
+		}
+
+		else {
+
+		 	//movieParamsName += movieTitle[i];
+
+		 	songName += songTitle[i];
+
+		}
+
+	} 
+
+	if ( !noSong ) {
+
+		songName = "The Sign";
+	}
+
+	spotify.search({type: 'track' , query: songName}, function(error,data,response){
+
+			var info = 
 
 				"Artist name: " + data.tracks.items[0].album.artists[0].name + "\r\n" + 
 
@@ -185,16 +254,72 @@ function doWhatItSays(){
 
 				"Album Name: " + data.tracks.items[0].album.name
 
-			);
-		});
+			console.log(info);
 
+			log(info);
 
 	});
 
-// End spotifyThisSong
+
+
 }
 
 
+function doWhatItSays(){
+
+	doWhat = true;
+
+	fs.readFile("random.txt", "utf8", function(error, data){
+
+		if (error) {
+			return console.log(error);
+		}
+
+		var results = data.split(",");
+
+		var songName =  results[1].replace(/"/g, "");
 
 
+		var clientId = neededKeys.spotifyKeys.client_id;
 
+		var clientSecret = neededKeys.spotifyKeys.client_secret;
+
+		var spotify = new spotifyApi({
+			id: clientId,
+			secret: clientSecret
+		});
+
+		spotify.search({type: 'track' , query: songName}, function(error,data,response){
+
+				var info = 
+
+					"Artist name: " + data.tracks.items[0].album.artists[0].name + "\r\n" + 
+
+					"Song Name: " + songName + "\r\n" +
+
+					"Preview Link: " +  data.tracks.items[0].href + "\r\n" +
+
+					"Album Name: " + data.tracks.items[0].album.name
+
+				console.log(info);
+
+				log(info);
+
+		});
+
+	});
+
+// End doWhatItSays
+}
+
+function log(data){
+
+	var logData = "\r\n"+data;
+
+	fs.appendFile("log.txt", logData, function(err) {
+	  if (err) {
+	    return console.log(err);
+	  }
+
+	});
+}
